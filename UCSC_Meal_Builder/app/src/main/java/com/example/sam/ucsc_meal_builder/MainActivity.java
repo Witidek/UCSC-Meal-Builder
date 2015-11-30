5f9cbeb858840999da6fcce5ed7a3060a0d7421c
Glenn Dayton <glenn.dayton24@gmail.com>
2015-11-30 00:18:07 -0800

    Added functionality for retrieving data from ucscmealbuilder.com/manage

--- UCSC_Meal_Builder/app/src/main/java/com/example/sam/ucsc_meal_builder/MainActivity.java
+++ UCSC_Meal_Builder/app/src/main/java/com/example/sam/ucsc_meal_builder/MainActivity.java
@@ -2,8 +2,8 @@ package com.example.sam.ucsc_meal_builder;
 
 import android.app.Activity;
 import android.content.Intent;
-
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
 import android.widget.TextView;
 
@@ -12,13 +12,23 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        /*
+            This should really be in its own thread, but i focused on getting it working first.
+         */
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+        StrictMode.setThreadPolicy(policy);
         setContentView(R.layout.activity_main);
         getActionBar().setDisplayShowTitleEnabled(false);
         getActionBar().setDisplayShowCustomEnabled(true);
         getActionBar().setCustomView(R.layout.ab_title);
         TextView title = (TextView) findViewById(android.R.id.text1);
         title.setText("UCSC Meal Builder");
-
+        //load data from url database.
+        //GetJSONForDB.execute()
+        new DBHelper(getApplicationContext()).getJSONFromURL("http://ucscmealbuilder.com/manage/api.php?type=r", 0);
+        new DBHelper(getApplicationContext()).getJSONFromURL("http://ucscmealbuilder.com/manage/api.php?type=i", 1);
     }
 
     public void onClickBuildMeal (View view) {
